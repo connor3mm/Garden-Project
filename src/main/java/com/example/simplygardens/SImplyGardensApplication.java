cce4ab21d01ebfbda43ead556ef3d89ed801a139
Connor McGuire SE2019 <connor.mcguire.2020@uni.strath.ac.uk>
2025-05-11 22:31:39 +0100

    -First commit, front end and backend created

---
+++ src/main/java/com/example/simplygardens/SImplyGardensApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplygardens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SImplyGardensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SImplyGardensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplygardens/SImplyGardensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplygardens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SImplyGardensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
